6e1377058f7c12539c39f676f187a3e9dfb12244
KirTeni02 <thekingvictor39@gmail.com>
2024-12-15 02:54:52 -0500

    lol

---
+++ js/moveareas.js
@@ -0,0 +1,9 @@
+function movetoForest() {
+  const principal = document.getElementById('principal-cont');
+  principal.style.transform = 'translateX(-50%)';
+}
+
+function movetoFactory() {
+  const principal = document.getElementById('principal-cont');
+  principal.style.transform = 'translateX(0)';
+}
